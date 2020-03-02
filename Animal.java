2d4f8bc5ca76a991f038ef4d548c458ddefb4e04
cottj1 <61671170+cottj1@users.noreply.github.com>
2020-03-01 21:04:09 -0500

    Add files via upload
    1st commit for Animal.java, Dog.java, MyCompare3.java

---
+++ Animal.java
@@ -0,0 +1,14 @@
+public class Animal extends MyCompare3 {
+	public int legs;
+	public String name;
+	
+	public Animal(String name, int legs) {
+		this.name = name;
+		this.legs = legs;
+	
+	}
+	
+	public int getLegs() {
+		return this.legs;
+	}
+}
---
+++ Dog.java
@@ -0,0 +1,10 @@
+
+public class Dog extends Animal{
+	public Dog(String name, int legs) {
+		super(name, legs);
+	}
+
+	public String toString() {
+		return name + legs;
+	}
+}
